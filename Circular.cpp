17cd2ba806b9b6189109cc9d6bbc6f905b00d811
bagusplendut-alt <bagusplendut@gmail.com>
2026-05-29 15:51:00 +0700

    implementasi logika pergeseran indeks

--- Circular.cpp
+++ Circular.cpp
@@ -15,7 +15,7 @@ public:
         REAR = -1;
     }
     void insert()
-    
+
     {
         int num;
         cout << "Enter a number: ";
@@ -29,3 +29,19 @@ public:
             return;
         }
 
+         // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else
+        {
+            // jika rear berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+    }
